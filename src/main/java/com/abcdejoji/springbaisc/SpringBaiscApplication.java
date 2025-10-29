70c6e0ba4b61748a45f9f288ea7039435099bedd
joji <abcdejoji@gmail.com>
2025-10-29 11:48:33 +0900

    init

---
+++ src/main/java/com/abcdejoji/springbaisc/SpringBaiscApplication.java
@@ -0,0 +1,13 @@
+package com.abcdejoji.springbaisc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBaiscApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBaiscApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/abcdejoji/springbaisc/SpringBaiscApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abcdejoji.springbaisc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBaiscApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
